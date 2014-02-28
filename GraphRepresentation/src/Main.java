89a20fa14f9661b762275ad7cd8f66d39ef8c2aa
hayk <hayk.baluyan@gmail.com>
2014-02-28 14:24:26 +0400

    Checking is there a cycle in undirected graph with DFS

--- GraphRepresentation/src/Main.java
+++ GraphRepresentation/src/Main.java
@@ -1,10 +1,13 @@
 import java.util.*;
 class Node<E>{
+	public enum Status {VISITED, NONVISITED};
 	private E id;
 	private ArrayList<Node<E>> adjNodes;
-	
+	private Status status;
+	private Node<E> parent;
 	public Node(E id){
 		this.id=id;
+		this.status=Status.NONVISITED;
 		adjNodes=new ArrayList<Node<E>>();
 	}
 	public void setId(E id){
@@ -13,7 +16,18 @@ class Node<E>{
 	public E getId(){
 		return id;
 	}
-
+	public void setStatus(Status status){
+		this.status=status;
+	}
+	public Status getStatus(){
+		return this.status;
+	}
+	public void setParent(Node<E> parent){
+		this.parent=parent;
+	}
+	public Node<E> getParent(){
+		return this.parent;
+	}
 	public void addAdjNodes(Node<E> ... nodes){
 		assert adjNodes!=null: "Adjacent list is NULL";
 		for(Node<E> n : nodes){
@@ -58,6 +72,36 @@ class Graph<E>{
 			iterator.next().printNode();
 		}
 	}
+	public boolean isCycle(){
+		if(nodes.size()==0){
+			return true;
+		}
+		boolean result=false;
+		Node<E> start=nodes.get(0);
+		start.setStatus(Node.Status.VISITED);
+		start.setParent(null);
+		LinkedList<Node<E>> queue=new LinkedList<Node<E>>();
+		queue.add(start);
+		while(queue.size()!=0){
+			Node<E> current=queue.removeFirst();
+			
+				for(Node<E> n : current.getAdjNodes()){
+					if(n.getStatus()==Node.Status.NONVISITED){
+						n.setStatus(Node.Status.VISITED);
+						n.setParent(current);
+						queue.add(n);
+					}
+					else{
+						if(current.getParent()!=n){
+						result=true;
+						break;
+						}
+					}
+				}
+			
+		}
+		return result;
+	}
 }
 public class Main {
 
@@ -68,17 +112,18 @@ public class Main {
 		Node<Integer> n3=new Node<Integer>(3);
 		Node<Integer> n4=new Node<Integer>(4);
 		Node<Integer> n5=new Node<Integer>(5);
-		n1.addAdjNodes(n2,n5);
-		n2.addAdjNodes(n1,n4);
+		n1.addAdjNodes(n2);
+		n2.addAdjNodes(n1,n5);
 		n3.addAdjNodes(n5);
 		n4.addAdjNodes(n2,n5);
-		n5.addAdjNodes(n1,n3,n4);
+		n5.addAdjNodes(n1,n2,n3,n4);
 		Graph G=new Graph();
 		G.addNodes(n1,n2,n3,n4,n5);
 		G.printGraph();
-		System.out.println("Remove edge between "+n1.getId()+" and "+n2.getId());
-		G.removeEdge(n1, n2);
-		G.printGraph();
+		//System.out.println("Remove edge between "+n1.getId()+" and "+n2.getId());
+		//G.removeEdge(n1, n2);
+		//G.printGraph();
+		System.out.println(G.isCycle());
 		
 		
 	}
